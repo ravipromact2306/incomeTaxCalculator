2638b1abb22e872b1f9f7dd45f8ab39a70c156ef
Ravi <raviagrawal@promactinfo.com>
2020-05-18 15:52:31 +0530

    initial commit

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ var gender = "";
 var income = 0.0;
 var tax = 0.0;
 
+var twoHalfLac = 250000;
+var threeLac = 300000;
+var fiveLac = 500000;
+var sevenHalfLac = 750000;
+var tenLac = 1000000;
+var tweleveHalfLac = 1250000;
+var fifteenLac = 1500000;
+var fifyLac = 500000;
+
 const readline = require('readline');
 
 const rl = readline.createInterface({
@@ -36,11 +45,11 @@ function calcualteIncomTax(){
 
     if(age > 80){
         console.log("above 80")
-        if (income < 500001) {
+        if (income < fifyLac) {
             tax = 0;
-        } else if (income > 1000000) {
-            tax = 37500 + (income - 750000) * 0.3;
-        } else if (income > 500000 && income < 1000000) {
+        } else if (income > tenLac) {
+            tax = 37500 + (income - tenLac) * 0.3;
+        } else if (income > 500000 && income < tenLac) {
             tax = 12500 + (income - 500000) * 0.2;
         } else {
             tax = 0;
@@ -48,34 +57,34 @@ function calcualteIncomTax(){
         
     }else if(age< 80 && age>60){
         console.log("60 to 80")
-        if (income < 500001) {
+        if (income < fifyLac) {
             tax = 0;
-        } else if (income > 1000000) {
-            tax = 37500 + (income - 750000) * 0.3;
-        } else if (income > 500000 && income < 1000000) {
-            tax = 12500 + (income - 500000) * 0.2;
-        } else if (income > 300000 && income < 500000) {
+        } else if (income > tenLac) {
+            tax = 37500 + (income - tenLac) * 0.3;
+        } else if (income > fiveLac && income < tenLac) {
+            tax = 12500 + (income - fiveLac) * 0.2;
+        } else if (income > threeLac && income < fiveLac) {
             console.log('this slab......');
-            tax = (income - 300000) * 0.05;
+            tax = (income - threeLac) * 0.05;
         } else {
             tax = 0;
         }
         
     }else{
-        if (income < 500001) {
+        if (income < fifyLac) {
             tax = 0;
-        } else if (income > 1500000) {
-            tax = 187500 + (income - 1500000) * 0.3;
-        } else if (income > 1250000) {
-            tax = 125000 + (income - 1250000) * 0.25;
-        } else if (income > 1000000) {
-            tax = 75000 + (income - 1000000) * 0.2;
-        } else if (income > 750000) {
-            tax = 37500 + (income - 750000) * 0.15;
-        } else if (income > 500000) {
-            tax = 12500 + (income - 500000) * 0.1;
-        } else if (income > 250000) {
-            tax = (income - 250000) * 0.05;
+        } else if (income > fifteenLac) {
+            tax = 187500 + (income - fifteenLac) * 0.3;
+        } else if (income > tweleveHalfLac) {
+            tax = 125000 + (income - tweleveHalfLac) * 0.25;
+        } else if (income > tenLac) {
+            tax = 75000 + (income - tenLac) * 0.2;
+        } else if (income > sevenHalfLac) {
+            tax = 37500 + (income - sevenHalfLac) * 0.15;
+        } else if (income > fiveLac) {
+            tax = 12500 + (income - fiveLac) * 0.1;
+        } else if (income > twoHalfLac) {
+            tax = (income - twoHalfLac) * 0.05;
         } else {
             tax = 0;
         }
